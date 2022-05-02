59d00819c775645de1aa623e1a7617b1cab0d9d2
adofsauron <adofsauron@163.com>
2022-05-02 17:18:16 +0800

    fix dev

--- samples/run_dbpack_samples.sh
+++ samples/run_dbpack_samples.sh
@@ -2,15 +2,15 @@
 
 mkdir -p logs
 
-pkill dbpack
-sleep 3s
+# pkill -15 dbpack
+# sleep 3s
 
-echo ../dist/dbpack start --config ../samples/go/config1.yaml 2>&1  | tee -a  ./logs/config1.log > ./logs/config1.log &
+echo "../dist/dbpack start --config ../samples/go/config1.yaml 2>&1  | tee -a  ./logs/config1.log > ./logs/config1.log &"
 ../dist/dbpack start --config ../samples/go/config1.yaml 2>&1  | tee -a  ./logs/config1.log > ./logs/config1.log &
 
-echo ../dist/dbpack start --config ../samples/go/config2.yaml 2>&1  | tee -a  ./logs/config2.log > ./logs/config2.log &
+echo "../dist/dbpack start --config ../samples/go/config2.yaml 2>&1  | tee -a  ./logs/config2.log > ./logs/config2.log &"
 ../dist/dbpack start --config ../samples/go/config2.yaml 2>&1  | tee -a  ./logs/config2.log > ./logs/config2.log &
 
-echo ../dist/dbpack start --config ../samples/go/config3.yaml 2>&1  | tee -a  ./logs/config3.log > ./logs/config3.log &
+echo "../dist/dbpack start --config ../samples/go/config3.yaml 2>&1  | tee -a  ./logs/config3.log > ./logs/config3.log &"
 ../dist/dbpack start --config ../samples/go/config3.yaml 2>&1  | tee -a  ./logs/config3.log > ./logs/config3.log &
 
